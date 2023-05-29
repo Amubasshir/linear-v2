b1953c5c005db0413654b8b37dd8359aabe86af1
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-29 12:14:36 +0600

    start illustration added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import classNames from 'classnames';
 import { Button, Highlight } from './components/Button';
 import { Container } from './components/Container';
 import { Hero, HeroSubtitle, HeroTitle } from './components/Hero';
 import Clients from './components/clients';
 import { HeroImage } from './components/hero-image';
 import { ChevronIcon } from './components/icons/Chevron';
+import { StarsIllustration } from './components/icons/Star';
 
 export default function Home() {
   return (
@@ -45,6 +47,15 @@ export default function Home() {
       </div>
       <Container>
         <Clients />
+        <div
+          className={classNames(
+            'relative my-[-12.8rem] h-[60rem] overflow-hidden',
+            '[--color:#7877C6] before:absolute before:inset-0 before:bg-radial-faded before:opacity-[0.4]',
+            'after:absolute after:-left-1/2 after:top-1/2 after:h-[142.8%] after:w-[200%] after:rounded-[50%] after:border-t  after:border-[rgba(120,_119,_198,_0.4)] after:bg-background'
+          )}
+        >
+          <StarsIllustration />
+        </div>
       </Container>
     </>
   );
